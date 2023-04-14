d4f395bf1961b2593a189202bffda1aa5a11b58f
HoaLeMinh <HoaLeMinh@users.noreply.github.com>
2023-04-14 21:21:52 +0700

    try changing next image to html image

--- components/HomeSection1.js
+++ components/HomeSection1.js
@@ -3,13 +3,19 @@ import Image from 'next/legacy/image'
 const HomeSection1 = ({ title, subtitle, imgSrc }) => (
   <div className="w-full space-y-2 pt-6 pb-8 md:space-y-5">
     <div className="relative">
-      <Image
+      {/* <Image
         className={'rounded-t-lg'}
         src={imgSrc}
         alt={title}
         width={'1920'}
         height={'560'}
         objectFit="cover"
+      /> */}
+      <img
+        src={imgSrc}
+        alt={title}
+        className="w-full"
+        style={{ height: '560', objectFit: 'cover' }}
       />
       <div className="absolute left-0 md:left-1/2 md:-translate-x-1/2 top-0 px-4 py-2 bg-gray-800 opacity-70">
         <h3 className="text-xl text-white font-bold text-center">{title}</h3>
